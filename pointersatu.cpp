a23e1b9fa5ef0c1d75f4e09e272c2115864ab417
spicyshibe <rafierasyw@gmail.com>
2025-05-06 15:18:52 +0700

    arrow

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -14,4 +14,14 @@ class mahasiswa
 int main()
 {
     mahasiswa mhs{1}; //objek mhs
+    mhs.showNim(); // memberi akses operator 
+
+    mahasiswa &refMhs = mhs; //pointer referensi mhs 
+    refMhs.nim = 2; //member akses operator
+    mhs.showNim(); 
+
+    mahasiswa *pMhs = &mhs; //pointer dereference pMhs
+    pMhs->nim = 3 ; //arrow operator 
+    pMhs->showNim();
+    return 0;
 }

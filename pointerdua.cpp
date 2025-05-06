cf29f70d354a8a085e40e2c0dfe4fb9f667173aa
spicyshibe <rafierasyw@gmail.com>
2025-05-06 15:25:05 +0700

    membuat pointer

--- pointerdua.cpp
+++ pointerdua.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 class mahasiswa
 {
-    
+    public:
+    int nim;
+    void showNim()
+    {
+        cout << "No Induk = " << nim << endl;
+
+    }
+};
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1}; //pointer object mhs
 }

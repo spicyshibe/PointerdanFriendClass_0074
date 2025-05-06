5563a1601c96093903a1536ad67a9e3b2576b27d
spicyshibe <rafierasyw@gmail.com>
2025-05-06 15:37:44 +0700

    void siswa

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -22,5 +22,13 @@ class siswa
     public:
     void setId(int pId);
     void displayAll(orang &a);
-    
+
+
+};
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+
 }
+void 
